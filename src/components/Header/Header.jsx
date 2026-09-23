59988124aae6a0ca0f56b983b695b23462462ea3
JOÃO PEDRO VIEIRA <joao_vieira160@estudante.sesisenai.org.br>
2026-09-23 17:05:47 -0300

    first commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,29 @@
+import "./Header.css";
 
-function Header(){
-    return(
-        <header className="header">  
-            <div className="logo">
-                <img className="logo-icon" src="#" alt="logo" />
-                <span className="logo-text">Studio alfa</span>
-            </div>
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-container">
+        <div className="logo">
+          <img
+            className="logo-icon"
+            src="../../../src/assets/img/kuromi.jpg"
+            alt="logo"
+          />
+          <span className="logo-text">Studio Alfa</span>
+        </div>
 
-            <nav className="nav">
-                <a href="#">inicio</a>
-                <a href="#">serviços</a>
-                <a href="#">sobre</a>
-                <a href="#" className="btn-contato">contato</a>
-            </nav>
-        </header>
-    )
-
+        <nav className="nav">
+          <a href="#">inicio</a>
+          <a href="#">serviços</a>
+          <a href="#">sobre</a>
+          <a href="#" className="btn-contatos">
+            contato
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
